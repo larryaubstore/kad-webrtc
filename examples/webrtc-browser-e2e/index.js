a2e54f497be14d7b31f7004b89cb0892cc7a840c
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2016-01-20 21:55:29 -0500

    e2e example updated.

--- examples/webrtc-browser-e2e/index.js
+++ examples/webrtc-browser-e2e/index.js
@@ -5,41 +5,70 @@ var WebRTC = require('../..');
 var webSocket = require('./web-socket');
 var SignalClient = require('./signal-client');
 var EventEmitter = require('events').EventEmitter;
-var signalClient1 = new SignalClient('node1');
-var signalClient2 = new SignalClient('node2');
 
-webSocket.on('open', function() {
+document.getElementById("nodeOne").onclick = function () {
 
+  alert("nodeOne");
+  var signalClient1 = new SignalClient('node1');
   // Create our first node
-  var node1 = new kademlia.Node({
+  window.node1 = new kademlia.Node({
     transport: new WebRTC(new WebRTC.Contact({
       nick: 'node1'
     }), { signaller: signalClient1 }),
     storage: new kademlia.storage.LocalStorage('node1')
   });
 
+
+//  node1.put('beep', 'boop', function(err) {
+//    if(err) {
+//        throw err;
+//    }
+//  });
+
+
+};
+
+document.getElementById("nodeTwo").onclick = function () {
+
+  alert("nodeTwo");
+  var signalClient2 = new SignalClient('node2');
   // Create a second node
-  var node2 = new kademlia.Node({
+  window.node2 = new kademlia.Node({
     transport: new WebRTC(new WebRTC.Contact({
       nick: 'node2'
     }), { signaller: signalClient2 }),
     storage: new kademlia.storage.LocalStorage('node2')
   });
 
-  node2.on('connect', onConnect);
+  window.node2.on('connect', onConnect);
 
-  node2.connect({ nick: 'node1' });
+  window.node2.connect({ nick: 'node1' });
+
 
   function onConnect() {
-    node1.put('beep', 'boop', onPut);
+      window.node2.put('beep', 'boop', function(err) {
+          if(err) {
+              throw err;
+          }
+      });
   }
 
-  function onPut(err) {
-    node2.get('beep', onGet);
-  }
+ 
+};
+
+
+document.getElementById("getButton").onclick = function () {
+
+  window.node1.get('beep', onGet);
 
   function onGet(err, value) {
     alert(value); // 'boop'
   }
+};
 
-});
+//webSocket.on('open', function() {
+//
+//
+//
+//
+//});
